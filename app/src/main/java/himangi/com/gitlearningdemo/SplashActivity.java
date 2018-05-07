78b4bd2d7e7b28ff764d5f858d25c7756904b057
Himangi Patel <himangi.patel@imobdev.com>
2018-05-07 18:11:45 +0530

    changes in master

--- app/src/main/java/himangi/com/gitlearningdemo/SplashActivity.java
+++ app/src/main/java/himangi/com/gitlearningdemo/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Log.i("Git Checkout", "SplashActivity dev-himangi");
+                Log.i("Git Checkout", "SplashActivity master");
 
             }
         }, 3000);
