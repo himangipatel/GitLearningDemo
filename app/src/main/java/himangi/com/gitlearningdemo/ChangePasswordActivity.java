14572bdbb20e4f9a2218f90b5bd0bcccb8e764b3
Himangi Patel <himangi.patel@imobdev.com>
2018-05-07 19:27:53 +0530

    add initHimangi method to dev-Himangi

--- app/src/main/java/himangi/com/gitlearningdemo/ChangePasswordActivity.java
+++ app/src/main/java/himangi/com/gitlearningdemo/ChangePasswordActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
  * Created by Darshna Desai on 7/5/18.
  */
 
-public class ChangePasswordActivity extends Activity{
+public class ChangePasswordActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        initHimangi();
+    }
+
+    private void initHimangi() {
+
     }
 }
