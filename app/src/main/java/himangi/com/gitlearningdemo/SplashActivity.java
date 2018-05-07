fd0bb20d9a924d5117c0c400b22868af6c58e6c3
Himangi Patel <himangi.patel@imobdev.com>
2018-05-07 11:13:20 +0530

    commit to dev1-himangi

--- app/src/main/java/himangi/com/gitlearningdemo/SplashActivity.java
+++ app/src/main/java/himangi/com/gitlearningdemo/SplashActivity.java
@@ -1,7 +1,9 @@
 package himangi.com.gitlearningdemo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Git Checkout", "test");
+
+            }
+        }, 3000);
     }
 }
