0404bb0d094821ff5da618a201b2ae0a3db43020
Himangi Patel <himangi.patel@imobdev.com>
2018-05-07 14:49:29 +0530

    change class name

---
+++ app/src/main/java/himangi/com/gitlearningdemo/LoginActivity.java
@@ -0,0 +1,28 @@
+package himangi.com.gitlearningdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.i("Git Checkout", "LoginActivity");
+
+        Log.i("Git Checkout", "LoginActivity");
+
+        Log.i("Git Checkout", "LoginActivity");
+
+        Log.i("Git Checkout", "LoginActivity Patch req2");
+
+
+        /// second pull req
+        Log.i("Git Checkout", "LoginActivity");
+
+        Log.i("Git Checkout", "LoginActivity");
+
+    }
+}
