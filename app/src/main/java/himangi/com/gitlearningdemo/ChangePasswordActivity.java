dfa8c689c266fa7104bd527786cb0e5127cf2442
GandhiRujul <rujulgandhi@yahoo.com>
2018-05-07 19:33:15 +0530

    init rujul method added

--- app/src/main/java/himangi/com/gitlearningdemo/ChangePasswordActivity.java
+++ app/src/main/java/himangi/com/gitlearningdemo/ChangePasswordActivity.java
@@ -2,16 +2,21 @@ package himangi.com.gitlearningdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by Darshna Desai on 7/5/18.
  */
 
-public class ChangePasswordActivity extends Activity{
+public class ChangePasswordActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+    }
+
+    public void initRujul() {
+        Log.d("dev-Rujul", "dev-Rujul");
     }
 }
